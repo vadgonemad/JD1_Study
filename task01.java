c7f9a2b31e3a58d3499b382d1cca5882b53689c1
vadgonemad <45932549+vadgonemad@users.noreply.github.com>
2019-01-08 18:46:41 +0300

    Add files via upload

---
+++ task01.java
@@ -0,0 +1,10 @@
+package tasks;
+
+public class task01 
+{
+	public static void main(String[] args) 
+	{	
+		int x=3, y=4; 		
+		System.out.println("сумма "+(x+y)+"разность "+(x-y)+"произведение "+(x*y)+"частное "+(x/y));
+	}
+}
